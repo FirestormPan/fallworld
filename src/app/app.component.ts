dac3629d62a6768446118b67ab3030d5791d7131
Pantelis <pantelos1999@gmail.com>
2026-01-26 15:22:03 +0200

    optimized memory

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GameStateServiceService } from './shared/services/game-state-service.service';
 import { AsyncPipe } from '@angular/common';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -10,30 +12,37 @@ import { AsyncPipe } from '@angular/common';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
 
   constructor(private gameService: GameStateServiceService) {}
   
-  highScore: number = 0; //TODO: save and load from local storage
+  highScore: number = this.gameService.getHighScore();
   title = 'fallworld';
   score: number = 0;
-
   gameEnded: boolean = false;
-
   paused$ = this.gameService.gameisPaused$;
   
+  private destroy$ = new Subject<void>();
 
   ngOnInit(){
-    this.gameService.score$.subscribe((currentScore)=>{
-      this.score = currentScore;
-    })
+    this.gameService.score$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((currentScore) => {
+        this.score = currentScore;
+      });
 
-    this.gameService.gameEnded$.subscribe((ended)=>{
-      this.gameEnded = ended;
-      this.highScore = this.gameService.getHighScore();
-    });
+    this.gameService.gameEnded$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((ended) => {
+        this.gameEnded = ended;
+        this.highScore = this.gameService.getHighScore();
+      });
   }
 
+  ngOnDestroy(){
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 
   pauseOrResume(){
     this.gameService.pauseOrResume();
@@ -42,7 +51,6 @@ export class AppComponent {
   playAgain(){
     this.gameService.resetGame();
   }
-
 }
 
 
